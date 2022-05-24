6f1fed8702a6e5ec585b45ff63be54ee04256043
FirinKinuo <deals@fkinuo.ru>
2022-05-24 14:49:42 +0300

    Add: Connect to Anilibria websocket and listen for response messages

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"altor/internal/settings"
+	"altor/internal/websocket"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -10,4 +11,14 @@ var cfg = settings.NewConfig()
 func main() {
 	settings.ConfigureLogger(cfg.LogLevel)
 	log.Info("Starting Altor!")
+
+	anilibriaWS, err := websocket.NewAnilibriaWS(cfg.AnilibriaWSURL.GetURL())
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	err = anilibriaWS.ListenSubscribes()
+	if err != nil {
+		log.Panicln(err)
+	}
 }
